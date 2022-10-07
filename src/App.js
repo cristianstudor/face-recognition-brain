08b505c8e103d80bad8ff519ad527b9ac03efb32
Cristian Tudor <cristi_tcs@live.com>
2022-10-07 16:16:56 +0300

    added heroku database

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://nameless-stream-48946.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ input: this.state.input })
@@ -87,7 +87,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://nameless-stream-48946.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({ id: this.state.user.id })
