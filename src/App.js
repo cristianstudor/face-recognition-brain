7f95b104db9b4dfd818e1a563bef2cda27fc75aa
Cristian Tudor <cristi_tcs@live.com>
2022-10-08 15:21:15 +0300

    small change

--- src/App.js
+++ src/App.js
@@ -27,20 +27,7 @@ const initialState = {
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input:'',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   calculateFaceLocation = (data) => {
